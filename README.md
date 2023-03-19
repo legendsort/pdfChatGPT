a3bd9a51201b2986947485de0e7314e0f32cfd67
Sortoite <zachatatem@gmail.com>
2023-03-19 12:06:07 -0400

    Feat: mvp of pdf gpt

---
+++ README.md
@@ -0,0 +1,56 @@
+
+# pdfGPT
+### Problem Description : 
+1. When you pass a large text to Open AI, it suffers from a 4K token limit. It cannot take an entire pdf file as an input
+2. Open AI sometimes becomes overtly chatty and returns irrelevant response not directly related to your query. This is because Open AI uses poor embeddings.
+3. ChatGPT cannot directly talk to external data. 
+
+### Solution: What is PDF GPT ?
+1. PDF GPT allows you to chat with an uploaded PDF file using GPT functionalities.
+2. The application intelligently breaks the document into smaller chunks and employs a powerful Deep Averaging Network Encoder to generate embeddings.
+3. A semantic search is first performed on your pdf content and the most relevant embeddings are passed to the Open AI.
+4. A custom logic generates precise responses. The returned response can even cite the page number in square brackets([]) where the information is located, adding credibility to the responses and helping to locate pertinent information quickly. The Responses are much better than the naive responses by Open AI.
+
+### Demo
+Demo URL: https://bit.ly/41ZXBJM
+
+**NOTE**: Please star this project if you like it!
+### UML
+```mermaid
+sequenceDiagram
+    participant User
+    participant System
+
+    User->>System: Enter API Key
+    User->>System: Upload PDF/PDF URL
+    User->>System: Ask Question
+    User->>System: Submit Call to Action
+
+    System->>System: Blank field Validations
+    System->>System: Convert PDF to Text
+    System->>System: Decompose Text to Chunks (150 word length)
+    System->>System: Check if embeddings file exists
+    System->>System: If file exists, load embeddings and set the fitted attribute to True
+    System->>System: If file doesn't exist, generate embeddings, fit the recommender, save embeddings to file and set fitted attribute to True
+    System->>System: Perform Semantic Search and return Top 5 Chunks with KNN
+    System->>System: Load Open AI prompt
+    System->>System: Embed Top 5 Chunks in Open AI Prompt
+    System->>System: Generate Answer with Davinci
+
+    System-->>User: Return Answer
+```
+
+### Flowchart
+```mermaid
+flowchart TB
+A[Input] --> B[URL]
+A -- Upload File manually --> C[Parse PDF]
+B --> D[Parse PDF] -- Preprocess --> E[Dynamic Text Chunks]
+C -- Preprocess --> E[Dynamic Text Chunks with citation history]
+E --Fit-->F[Generate text embedding with Deep Averaging Network Encoder on each chunk]
+F -- Query --> G[Get Top Results]
+G -- K-Nearest Neighbour --> K[Get Nearest Neighbour - matching citation references]
+K -- Generate Prompt --> H[Generate Answer]
+H -- Output --> I[Output]
+```
+
---
+++ app.py
@@ -0,0 +1,238 @@
+import urllib.request
+import fitz
+import re
+import numpy as np
+import tensorflow_hub as hub
+import openai
+import gradio as gr
+import os
+from sklearn.neighbors import NearestNeighbors
+
+def download_pdf(url, output_path):
+    urllib.request.urlretrieve(url, output_path)
+
+
+def preprocess(text):
+    text = text.replace('\n', ' ')
+    text = re.sub('\s+', ' ', text)
+    return text
+
+
+def pdf_to_text(path, start_page=1, end_page=None):
+    doc = fitz.open(path)
+    total_pages = doc.page_count
+
+    if end_page is None:
+        end_page = total_pages
+
+    text_list = []
+
+    for i in range(start_page-1, end_page):
+        text = doc.load_page(i).get_text("text")
+        text = preprocess(text)
+        text_list.append(text)
+
+    doc.close()
+    return text_list
+
+
+def text_to_chunks(texts, word_length=150, start_page=1):
+    text_toks = [t.split(' ') for t in texts]
+    page_nums = []
+    chunks = []
+    
+    for idx, words in enumerate(text_toks):
+        for i in range(0, len(words), word_length):
+            chunk = words[i:i+word_length]
+            if (i+word_length) > len(words) and (len(chunk) < word_length) and (
+                len(text_toks) != (idx+1)):
+                text_toks[idx+1] = chunk + text_toks[idx+1]
+                continue
+            chunk = ' '.join(chunk).strip()
+            chunk = f'[{idx+start_page}]' + ' ' + '"' + chunk + '"'
+            chunks.append(chunk)
+    return chunks
+
+
+class SemanticSearch:
+    
+    def __init__(self):
+        self.use = hub.load('https://tfhub.dev/google/universal-sentence-encoder/4')
+        self.fitted = False
+    
+    
+    def fit(self, data, batch=1000, n_neighbors=5):
+        self.data = data
+        self.embeddings = self.get_text_embedding(data, batch=batch)
+        n_neighbors = min(n_neighbors, len(self.embeddings))
+        self.nn = NearestNeighbors(n_neighbors=n_neighbors)
+        self.nn.fit(self.embeddings)
+        self.fitted = True
+    
+    
+    def __call__(self, text, return_data=True):
+        inp_emb = self.use([text])
+        neighbors = self.nn.kneighbors(inp_emb, return_distance=False)[0]
+        
+        if return_data:
+            return [self.data[i] for i in neighbors]
+        else:
+            return neighbors
+    
+    
+    def get_text_embedding(self, texts, batch=1000):
+        embeddings = []
+        for i in range(0, len(texts), batch):
+            text_batch = texts[i:(i+batch)]
+            emb_batch = self.use(text_batch)
+            embeddings.append(emb_batch)
+        embeddings = np.vstack(embeddings)
+        return embeddings
+
+
+
+#def load_recommender(path, start_page=1):
+#    global recommender
+#   texts = pdf_to_text(path, start_page=start_page)
+#   chunks = text_to_chunks(texts, start_page=start_page)
+#    recommender.fit(chunks)
+#    return 'Corpus Loaded.'
+
+# The modified function generates embeddings based on PDF file name and page number and checks if the embeddings file exists before loading or generating it.	
+def load_recommender(path, start_page=1):
+    global recommender
+    pdf_file = os.path.basename(path)
+    embeddings_file = f"{pdf_file}_{start_page}.npy"
+    
+    if os.path.isfile(embeddings_file):
+        embeddings = np.load(embeddings_file)
+        recommender.embeddings = embeddings
+        recommender.fitted = True
+        return "Embeddings loaded from file"
+    
+    texts = pdf_to_text(path, start_page=start_page)
+    chunks = text_to_chunks(texts, start_page=start_page)
+    recommender.fit(chunks)
+    np.save(embeddings_file, recommender.embeddings)
+    return 'Corpus Loaded.'
+
+
+
+def generate_text(openAI_key,prompt, engine="text-davinci-003"):
+    openai.api_key = openAI_key
+    completions = openai.Completion.create(
+        engine=engine,
+        prompt=prompt,
+        max_tokens=512,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    message = completions.choices[0].text
+    return message
+
+
+def generate_answer(question,openAI_key):
+    topn_chunks = recommender(question)
+    prompt = ""
+    prompt += 'search results:\n\n'
+    for c in topn_chunks:
+        prompt += c + '\n\n'
+        
+    prompt += "Instructions: Compose a comprehensive reply to the query using the search results given. "\
+              "Cite each reference using [number] notation (every result has this number at the beginning). "\
+              "Citation should be done at the end of each sentence. If the search results mention multiple subjects "\
+              "with the same name, create separate answers for each. Only include information found in the results and "\
+              "don't add any additional information. Make sure the answer is correct and don't output false content. "\
+              "If the text does not relate to the query, simply state 'Found Nothing'. Ignore outlier "\
+              "search results which has nothing to do with the question. Only answer what is asked. The "\
+              "answer should be short and concise.\n\nQuery: {question}\nAnswer: "
+    
+    prompt += f"Query: {question}\nAnswer:"
+    answer = generate_text(openAI_key, prompt,"text-davinci-003")
+    return answer
+
+
+def question_answer(url, file, question,openAI_key):
+    if openAI_key.strip()=='':
+        return '[ERROR]: Please enter you Open AI Key. Get your key here : https://platform.openai.com/account/api-keys'
+    if url.strip() == '' and file == None:
+        return '[ERROR]: Both URL and PDF is empty. Provide atleast one.'
+    
+    if url.strip() != '' and file != None:
+        return '[ERROR]: Both URL and PDF is provided. Please provide only one (eiter URL or PDF).'
+
+    if url.strip() != '':
+        glob_url = url
+        download_pdf(glob_url, 'corpus.pdf')
+        load_recommender('corpus.pdf')
+
+    else:
+        old_file_name = file.name
+        file_name = file.name
+        file_name = file_name[:-12] + file_name[-4:]
+        os.rename(old_file_name, file_name)
+        load_recommender(file_name)
+
+    if question.strip() == '':
+        return '[ERROR]: Question field is empty'
+
+    return generate_answer(question,openAI_key)
+
+
+recommender = SemanticSearch()
+
+title = 'PDF GPT'
+description = """ What is PDF GPT ?
+1. The problem is that Open AI has a 4K token limit and cannot take an entire PDF file as input. Additionally, it sometimes returns irrelevant responses due to poor embeddings. ChatGPT cannot directly talk to external data. The solution is PDF GPT, which allows you to chat with an uploaded PDF file using GPT functionalities. The application breaks the document into smaller chunks and generates embeddings using a powerful Deep Averaging Network Encoder. A semantic search is performed on your query, and the top relevant chunks are used to generate a response.
+2. The returned response can even cite the page number in square brackets([]) where the information is located, adding credibility to the responses and helping to locate pertinent information quickly. The Responses are much better than the naive responses by Open AI."""
+
+with gr.Blocks() as demo:
+
+    gr.Markdown(f'<center><h1>{title}</h1></center>')
+    gr.Markdown(description)
+
+    with gr.Row():
+        
+        with gr.Group():
+            gr.Markdown(f'<p style="text-align:center">Get your Open AI API key <a href="https://platform.openai.com/account/api-keys">here</a></p>')
+            openAI_key=gr.Textbox(label='Enter your OpenAI API key here')
+            url = gr.Textbox(label='Enter PDF URL here')
+            gr.Markdown("<center><h4>OR<h4></center>")
+            file = gr.File(label='Upload your PDF/ Research Paper / Book here', file_types=['.pdf'])
+            question = gr.Textbox(label='Enter your question here')
+            btn = gr.Button(value='Submit')
+            btn.style(full_width=True)
+
+        with gr.Group():
+            answer = gr.Textbox(label='The answer to your question is :')
+
+        btn.click(question_answer, inputs=[url, file, question,openAI_key], outputs=[answer])
+#openai.api_key = os.getenv('Your_Key_Here') 
+demo.launch()
+
+
+# import streamlit as st
+
+# #Define the app layout
+# st.markdown(f'<center><h1>{title}</h1></center>', unsafe_allow_html=True)
+# st.markdown(description)
+
+# col1, col2 = st.columns(2)
+
+# # Define the inputs in the first column
+# with col1:
+#     url = st.text_input('URL')
+#     st.markdown("<center><h6>or<h6></center>", unsafe_allow_html=True)
+#     file = st.file_uploader('PDF', type='pdf')
+#     question = st.text_input('question')
+#     btn = st.button('Submit')
+
+# # Define the output in the second column
+# with col2:
+#     answer = st.text_input('answer')
+
+# # Define the button action
+# if btn:
+#     answer_value = question_answer(url, file, question)
+#     answer.value = answer_value
